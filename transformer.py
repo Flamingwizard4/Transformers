a6760f034e9b35930b847e67418dd7f869202fe3
Flamingwizard4 <flamingwizard4@gmail.com>
2019-11-19 13:08:02 -0500

    Add files via upload

--- transformer.py
+++ transformer.py
@@ -15,9 +15,15 @@ import math, copy, time, os, pdb
 from torch.autograd import Variable
 import matplotlib.pyplot as plt
 import seaborn as sns
+from torchtext import data, datasets
+import spacy
 
 sns.set_context(context="talk")
 #%matplotlib inline
+
+
+"""METHODS"""
+
 
 def clones(module, N):
     #produces N identical layers
@@ -52,6 +58,20 @@ def attention(query, key, value, mask=None, dropout=None):
     is similar to that of single-head attention with full dimensionality (512).
     """
 
+global max_src_in_batch, max_tgt_in_batch
+def batch_size_func(new, count, sofar): #is this designed for the dataset or for general use??
+    #keeps augmenting batch and calculating total number of tokens + padding
+    global max_src_in_batch, max_tgt_in_batch #why twice?? once in scope and once not
+    if count == 1:
+        max_src_in_batch = 0
+        max_tgt_in_batch = 0
+    max_src_in_batch = max(max_src_in_batch, len(new.src))
+    max_tgt_in_batch = max(max_tgt_in_batch, len(new.tgt) + 2)
+    src_elem = count * max_src_in_batch
+    tgt_elem = count * max_tgt_in_batch
+    return max(src_elem, tgt_elem)
+
+
 def make_model(src_vocab, tgt_vocab, N=6, d_model=512, d_ff=2048, h=8, dropout=0.1):
     """
     Contructs model from hyperparameters:
@@ -74,13 +94,38 @@ def make_model(src_vocab, tgt_vocab, N=6, d_model=512, d_ff=2048, h=8, dropout=0
         nn.Sequential(Embeddings(d_model, tgt_vocab), c(pos)),
         Generator(d_model, tgt_vocab))
 
-    #This was important/imported from their code
+    #This was important (imported*?) from their code
     #Initialize parameters with Glorot / fan_avg
     for p in model.parameters():
         if p.dim() > 1:
-            nn.init.xavier_uniform(p)
+            nn.init.xavier_uniform_(p)
 
     return model
+
+def run_epoch(data_iterator, model, loss_comp):
+    #standard training/logging function
+    start = time.time()
+    total_toks = 0
+    total_loss = 0
+    toks = 0
+
+    for i, batch in enumerate(data_iterator):
+        out = model.forward(batch.src, batch.tgt, batch.src_mask, batch.tgt_mask)
+        loss = loss_comp(out, batch.tgt_y, batch.ntokens)
+        total_loss += loss
+        total_toks += batch.ntokens
+        tokens += batch.ntokens
+        if i % 50 == 1:
+            elapsed = time.time() - start
+            print("Epoch Step: %d   Loss: %f   Tokens per Second: %f" %
+                    (i, loss / batch.ntokens, tokens / elapsed))
+            start = time.time()
+            tokens = 0
+
+    return total_loss / total_toks
+
+
+"""OBJECTS"""
 
 
 class EncoderDecoder(nn.Module):
@@ -90,11 +135,11 @@ class EncoderDecoder(nn.Module):
     """
     def __init__(self, enc, dec, src_embed, tgt_embed, gen):
         super(EncoderDecoder, self).__init__()
-        self.encoder = encoder
-        self.decoder = decoder
+        self.encoder = enc
+        self.decoder = dec
         self.src_embed = src_embed
         self.tgt_embed = tgt_embed
-        self.generator = generator
+        self.generator = gen
 
     def forward(self, src, tgt, src_mask, tgt_mask):
         #take in and process masked src and tgt sequences
@@ -108,23 +153,13 @@ class EncoderDecoder(nn.Module):
         return self.decoder(self.tgt_embed(tgt), memory, src_mask, tgt_mask)
 
 
-class Generator(nn.Module):
-    #defines standard linear + softmax generation step (the end I think)
-    def __init__(self, d_model, vocab):
-        super(Generator, self).__init__()
-        self.proj = nn.Linear(d_model, vocab) #torch.nn.Linear(in_features, out_features, bias=True)
-
-    def forward(self, x):
-        return F.log_softmax(self.proj(x), dim = -1) #why the use of a different softmax here at the end??
-
-
 class LayerNorm(nn.Module): #used in SublayerConnection & core Encoder/Decoder classes
     #constructs a layer normalization module
     def __init__(self, features, eps=1e-6):
         super(LayerNorm, self).__init__()
         self.a_2 = nn.Parameter(torch.ones(features))
         self.b_2 = nn.Parameter(torch.zeros(features))
-        slef.eps = eps
+        self.eps = eps
 
     def forward(self, x):
         mean = x.mean(-1, keepdim=True)
@@ -144,6 +179,7 @@ class Encoder(nn.Module):
         for layer in self.layers:
             x = layer(x, mask)
         return self.norm(x)
+
 
 class Decoder(nn.Module):
     #Generic N layer decoder with masking
@@ -203,58 +239,45 @@ class DecoderLayer(nn.Module):
         return self.sublayer[2](x, self.feed_forward)
 
 
-class MultiHeadAttn(nn.Module):
-    #used for decoding/src_attn, encoding/self_attn, and decoding/self_attn i.e. autoregression
-    def __init__(self, heads, d_model, dropout=0.1):
-        super(MultiHeadAttn, self).__init__()
-        assert d_model % heads == 0
-        #We assume d_v always equals d_k
-        self.d_k = d_model // heads
-        self.h = heads
-        self.linears = clones(nn.Linear(d_model, d_model), 4)
-        self.attn = None
-        self.dropout = nn.Dropout(p=dropout)
+class Batch:
+    #object to hold a batch of data with mask during training
+    def __init__(self, src, tgt=None, pad=0):
+        self.src = src
+        self.src_mask = (src != pad).unsqueeze(-2)
+        if tgt is not None:
+            self.tgt = tgt[:, :-1]
+            self.tgt_y = tgt[:, 1:]
+            self.tgt_mask = self.make_std_mask(self.tgt, pad)
+            self.ntokens = (self.tgt_y != pad).data.sum()
 
-    def forward(self, query, key, value, mask=None):
-        if mask is not None:
-            #same mask applied to all h heads
-            mask = mask.unsqueeze(1)
-        n_batches = query.size(0)
+    def make_std_mask(tgt, pad):
+        #creates a mask to hide padding and future words
+        tgt_mask = (tgt != pad).unsqueeze(-2)
+        tgt_mask = tgt_mask & Variable(
+            subsequent_mask(tgt.size(-1)).type_as(tgt_mask.data))
+        return tgt_mask
 
-        #do all linear projections in batch from d_model => h x d_k
-        query, key, value = [l(x).view(n_batches, -1, self.d_k).transpose(1, 2)
-        for l, x in zip(self.linears, (query, key, value))]
 
-        #apply attention on all projected vectors in the batch
-        x, self.attn = attention(query, key, value, mask=mask, dropout=self.dropout)
+class MyIterator(data.Iterator): #creates batches
+    def create_batches(self):
+        if self.train:
+            def pool(d, ran_shuffler):
+                for p in data.batch(d, self.batch_size * 100):
+                    p_batch = data.batch(
+                        sorted(p, key=self.sort_key),
+                        self.batch_size, self.batch_size_func)
+                    for b in ran_shuffler(list(p_batch)):
+                        yield b
+            self.batches = pool(self.data(), self.ran_shuffler)
+        else:
+            self.batches = []
+            for b in data.batch(self.data(), self.batch_size, self.batch_size_func):
+                self.batches.append(sorted(b, key=self.sort_key))
 
-        #"concat" using a view and apply a final linear
-        x = x.transpose(1, 2).contiguous().view(n_batches, -1, self.h * self.d_k)
-
-        return self.linears[-1](x)
-
-class PositionwiseFFLayer(nn.Module):
-    """
-    In addition to attention sub-layers, each of the layers in our encoder and decoder
-    contains a fully connected feed-forward network, which is applied to each position
-    separately and identically. This consists of two linear transformations with a
-    ReLU activation in between.
-
-    Implements FFN equation FFN(x) = max(0, xW_1 + b_1)W_2 + b_2
-
-    While the linear transformations are the same across different positions, they use
-    different parameters from layer to layer. Another way of describing this is as
-    two convolutions with kernel size 1. The dimensionality of input and output is
-    d_model = 512, and the inner-layer has dimensionality d_ff = 2048.
-    """
-    def __init__(self, d_model, d_ff, dropout=0.1):
-        super(PositionwiseFFLayer, self).__init__()
-        self.w_1 = nn.Linear(d_model, d_ff)
-        self.w_2 = nn.Linear(d_ff, d_model)
-        self.dropout = nn.Dropout(dropout)
-
-    def forward(self, x):
-        return self.w_2(self.dropout(F.relu(self.w_1(x))))
+def rebatch(pad_ndx, batch):
+    #fixes order in torchtext to match ours
+    src, tgt = batch.src.transpose(0, 1), batch.tgt.transpose(0, 1)
+    return Batch(src, tgt, pad_ndx)
 
 
 class Embeddings(nn.Module):
@@ -301,9 +324,9 @@ class PositionalEncoding(nn.Module):
         self.dropout = nn.Dropout(p=dropout)
 
         #compute positional encodings once in log space
-        pe = torch.zeros(max_len, d_model)
-        pos = torch.arange(0, max_len).unsqueeze(1)
-        div_term = torch.exp(torch.arange(0, d_model, 2) * -(math.log(10000.0) / d_model)) #this is a LongTensor instead of a FloatTensor for some reason
+        pe = torch.zeros(max_len, d_model, dtype = torch.float32)
+        pos = torch.arange(0, max_len).unsqueeze(1).float()
+        div_term = torch.exp((torch.arange(0, d_model, 2) * -(math.log(10000.0) / d_model)).float())
         pe[:, 0::2] = torch.sin(pos * div_term)
         pe[:, 1::2] = torch.cos(pos * div_term)
         pe = pe.unsqueeze(0)
@@ -314,4 +337,225 @@ class PositionalEncoding(nn.Module):
         return self.dropout(x)
 
 
-tmp_model = make_model(10, 10, 2)
+class PositionwiseFFLayer(nn.Module):
+    """
+    In addition to attention sub-layers, each of the layers in our encoder and decoder
+    contains a fully connected feed-forward network, which is applied to each position
+    separately and identically. This consists of two linear transformations with a
+    ReLU activation in between.
+
+    Implements FFN equation FFN(x) = max(0, xW_1 + b_1)W_2 + b_2
+
+    While the linear transformations are the same across different positions, they use
+    different parameters from layer to layer. Another way of describing this is as
+    two convolutions with kernel size 1. The dimensionality of input and output is
+    d_model = 512, and the inner-layer has dimensionality d_ff = 2048.
+    """
+    def __init__(self, d_model, d_ff, dropout=0.1):
+        super(PositionwiseFFLayer, self).__init__()
+        self.w_1 = nn.Linear(d_model, d_ff)
+        self.w_2 = nn.Linear(d_ff, d_model)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x):
+        return self.w_2(self.dropout(F.relu(self.w_1(x))))
+
+
+class Generator(nn.Module):
+    #defines standard linear + softmax generation step (the end I think)
+    def __init__(self, d_model, vocab):
+        super(Generator, self).__init__()
+        self.proj = nn.Linear(d_model, vocab) #torch.nn.Linear(in_features, out_features, bias=True)
+
+    def forward(self, x):
+        return F.log_softmax(self.proj(x), dim = -1) #why the use of a different softmax here at the end??
+
+
+class MultiHeadAttn(nn.Module):
+    """
+    Used For:
+    decoding/src_attn i.e. every position in the decoder attends over all positions in input sequence
+    encoding/self_attn i.e. each position in the encoder attends to all positions in the previous layer of enc
+    decoding/self_attn i.e. same as above but with subsequent masking for autoregression
+    """
+    def __init__(self, heads, d_model, dropout=0.1):
+        super(MultiHeadAttn, self).__init__()
+        assert d_model % heads == 0
+        #We assume d_v always equals d_k
+        self.d_k = d_model // heads
+        self.h = heads
+        self.linears = clones(nn.Linear(d_model, d_model), 4)
+        self.attn = None
+        self.dropout = nn.Dropout(p=dropout)
+
+    def forward(self, query, key, value, mask=None):
+        if mask is not None:
+            #same mask applied to all h heads
+            mask = mask.unsqueeze(1)
+        n_batches = query.size(0)
+
+        #do all linear projections in batch from d_model => h x d_k
+        query, key, value = [l(x).view(n_batches, -1, self.d_k).transpose(1, 2)
+        for l, x in zip(self.linears, (query, key, value))]
+
+        #apply attention on all projected vectors in the batch
+        x, self.attn = attention(query, key, value, mask=mask, dropout=self.dropout)
+
+        #"concatenation" using a view and application of a final linear
+        x = x.transpose(1, 2).contiguous().view(n_batches, -1, self.h * self.d_k)
+
+        return self.linears[-1](x)
+
+
+class NoamOpt:
+    #optimizer wrapper implementing rate
+    def __init__(self, model_size, fac, warm, optim):
+        self.optimizer = optim
+        self.warmup = warm
+        self.factor = fac
+        self.model_size = model_size
+        self._step = 0
+        self._rate = 0
+
+    def step(self):
+        #updates parameters and rate
+        self._step += 1
+        rate = self.rate()
+        for p in self.optimizer.param_groups:
+            p['lr'] = rate
+        self._rate = rate
+        self.optimizer.step()
+
+    def rate(self, step = None):
+        #determines learning rate
+        if step is None:
+            step = self._step
+        return self.factor * (self.model_size ** (-0.5) * min(step ** (-0.5), step * self.warmup ** (-1.5)))
+
+def get_std_opt(model): #returns optimizer wrapper with given hyperparameters
+    return NoamOpt(model.src_embed[0].d_model, 2, 4000,
+            torch.optim.Adam(model.parameters(), lr=0, betas=(0.9, 0.98), eps=1e-9))
+
+
+class LabelSmooth(nn.Module):
+    """
+    Implements label smoothing with default value 0.1.
+    This hurts perplexity but improves accuracy/BLEU
+    as the model learns to be more unsure.
+
+    Instead of using a one-hot target distribution,
+    we create a distribution that has confidence of the correct word
+    and the rest of the smoothing mass distributed throughout the vocabulary.
+    """
+    def __init__(self, size, pad_ndx, smoothing=0.0):
+        super(LabelSmooth, self).__init__()
+        self.criterion = nn.KLDivLoss(size_average=False)
+        self.pad_ndx = pad_ndx
+        self.confidence = 1.0 - smoothing
+        self.smoothing = smoothing
+        self.size = size
+        self.true_dist = None
+
+    def forward(self, x, tgt):
+        assert x.size(1) == self.size
+        true_dist = x.data.clone()
+        true_dist.fill_(self.smoothing / (self.size - 2))
+        true_dist.scatter_(1, tgt.data.unsqueeze(1), self.confidence)
+        true_dist[:, self.pad_ndx] = 0
+        mask = torch.nonzero(tgt.data == self.pad_ndx)
+        if mask.dim() > 0:
+            true_dist.index_fill_(0, mask.squeeze(), 0.0)
+        self.true_dist = true_dist
+        return self.criterion(x, Variable(true_dist, requires_grad=False))
+
+
+class SimpleLossComp:
+    #simple loss compute & train function
+    def __init__(self, gen, crit, opt=None): #opt is optimizer wrapper
+        self.generator = gen
+        self.criterion = crit
+        self.opt = opt
+
+    def __call__(self, x, y, norm):
+        x = self.generator(x)
+        loss = self.criterion(x.contiguous().view(-1, x.size(-1)),
+                              y.contiguous().view(-1)) / norm
+        loss.backward()
+        if self.opt is not None:
+            self.opt.step()
+            self.opt.optimizer.zero_grad()
+        return loss.data[0] * norm
+
+
+class MultiGPULossComp:
+    #multi GPU loss function
+    def __init__(self, gen, crit, devices, opt=None, chunk_size=5):
+        #send out to different GPUs
+        self.generator = gen
+        self.criterion = nn.parallel.replicate(crit, devices=devices)
+        self.opt = opt
+        self.devices = devices
+        self.chunk_size = chunk_size
+
+    def __call__(self, out, tgts, norm):
+        total = 0.0
+        generator = nn.parallel.replicate(self.generator, devices=self.devices)
+        out_scatter = nn.parallel.scatter(out, target_gpus=self.devices)
+        out_grad = [[] for _ in out_scatter]
+        targets = nn.parallel.scatter(tgts, target_gpus=self.devices)
+
+        #divide generator into chunks
+        chunk_size = self.chunk_size
+        for i in range(0, out_scatter[0].size(1), chunk_size):
+            #predict distributions
+            out_col = [[Variable(o[:, i:i+chunk_size].data,
+                                 requires_grad=(self.opt is not None))]
+                        for o in out_scatter]
+            gen = nn.parallel.parallel_apply(generator, out_col)
+
+            #compute loss
+            y = [(g.contiguous().view(-1, g.size(-1)),
+                  t[:, i:i+chunk_size].contiguous().view(-1))
+                 for g, t in zip(gen, tgts)]
+            loss = nn.parallel.parallel_apply(self.criterion, y)
+
+            #sum & normalize loss
+            l = nn.parallel.gather(loss, tgt_devices=self.devices[0])
+            l = l.sum()[0] / norm
+            total += l.data[0]
+
+            #backpropagate loss to output of transformer
+
+            """CONTINUE WORK HERE!!!"""
+
+
+def load_data():
+    spacy_de = spacy.load('de')
+    spacy_en = spacy.load('en')
+
+    def tokenize_de(text):
+        return [tok.text for tok in spacy_de.tokenizer(text)]
+
+    def tokenize_en(text):
+        return [tok.text for tok in spacy_en.tokenizer(text)]
+
+    BOS_WORD = '<s>'
+    EOS_WORD = '</s>'
+    BLANK_WORD = "<blank>"
+    SRC = data.Field(tokenize=tokenize_de, pad_token=BLANK_WORD)
+    TGT = data.Field(tokenize=tokenize_en, init_token = BOS_WORD,
+                     eos_token = EOS_WORD, pad_token=BLANK_WORD)
+
+    MAX_LEN = 100
+    train, val, test = datasets.IWSLT.splits(
+        exts=('.de', '.en'), fields=(SRC, TGT),
+        filter_pred=lambda x: len(vars(x)['src']) <= MAX_LEN and
+            len(vars(x)['trg']) <= MAX_LEN)
+    MIN_FREQ = 2
+    SRC.build_vocab(train.src, min_freq=MIN_FREQ)
+    TGT.build_vocab(train.trg, min_freq=MIN_FREQ)
+
+
+
+
+basic_model = make_model(10, 10, 2)
